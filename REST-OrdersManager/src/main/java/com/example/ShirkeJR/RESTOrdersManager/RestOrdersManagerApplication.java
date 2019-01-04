410ec26a4ce10bfc11fb6038e12bcacead2f8648
ShirkeJR <asmax6@gmail.com>
2019-01-04 12:32:24 +0100

    init api

---
+++ REST-OrdersManager/src/main/java/com/example/ShirkeJR/RESTOrdersManager/RestOrdersManagerApplication.java
@@ -0,0 +1,14 @@
+package com.example.ShirkeJR.RESTOrdersManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestOrdersManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestOrdersManagerApplication.class, args);
+	}
+
+}
+
